da4e058d7f7df66179c4c2a6869bdda8aad4dbb4
Moglix <kumarsanjaysk852@gmail.com>
2025-03-20 21:49:13 +0530

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './user/users.module';
 
-const ENV = process.env.NODE_ENV; // Default to 'qa' for local
+// const ENV = process.env.NODE_ENV; // Default to 'qa' for local
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.env.${ENV}`,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
       isGlobal: true
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI || ''), // process.env = envFilePath =`.env.${ENV}` = .env.prod
